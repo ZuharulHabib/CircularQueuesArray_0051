ef8f437d59fdc4fb3c618d07bf9155fc9e54bfce
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2024-05-31 14:40:44 +0700

    Menampilkan pesan jika antrian kosong             return; // Keluar dari fungsi

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -42,7 +42,7 @@ public:
 	void remove() {
 		//Cek apakah antrian kosong
 		if (FRONT == -1) {
-			cout << "Queue underflow\n";
+			cout << "Queue underflow\n"; 
 			return;
 		}
 		cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
